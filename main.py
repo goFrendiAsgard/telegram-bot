6dc174b924795b3265680a6cb111b96ead179cf8
goFrendiAsgard <gofrendiasgard@gmail.com>
2021-09-06 17:37:11 +0700

    add functionality

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandle
 def hello(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
+def add(update: Update, context: CallbackContext) -> None:
+    user_message = update.message.text
+    cmd, n1, n2 = user_message.split(" ")
+    result = float(n1) + float(n2)
+    update.message.reply_text(f'{n1} + {n2} = {result}')
+
 
 updater = Updater(os.getenv('TELEGRAM_TOKEN'))
-
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('add', add))
 
 
 updater.start_polling()
